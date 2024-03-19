01a2ba07be03f0baca45f3acd10db34ac4ecab95
younus amshan <younusamshankhan@gmail.com>
2024-03-19 06:07:55 +0500

    book asignment

---
+++ app.js
@@ -0,0 +1,567 @@
+//document.write("hello word");
+//console.log("22"-2);
+// alert("hello"<br>"word"four);
+
+// var inputUser = "koch b"
+
+// var item = ["one", "two", "three", "four", "five",]
+// var input = prompt("kon ho aip")
+// console.log(user ko ${input} ka kahana ha ${input[aaaa]}ha)
+// prompt(inputUser)
+
+
+// const fastName = "younus"
+// let lastName = "amshan"
+//  fastName = "khan"
+//  console.log(fastName + " " + "amshan")
+// { const name = "khan amshan" }
+// console.log("amshan" + name)
+
+// var items = ["Qorma", "nehari", "chefs", "karahi",]
+// console.log( `items ${3}`)
+// var width = +prompt("aip apna  pekhla rakam yaha liko");
+// var height = +prompt("aip apna Dosra Rakam yaha liko ");
+
+
+// var totelSize = (width + height)
+// document.write("<h1>totel </h1>" + totelSize)
+
+
+// var name = akash;
+// var userName = prompt("plaese Enter Your name");
+// var userage = prompt("plaese Enter Your Age");
+
+
+
+
+
+// if(userage <= 14){
+//     console.log("aip Chote Yaar hu")
+// }
+
+// if(userage <= 18 && userage >= 15){
+//     console.log("aip School student hu")
+// }
+
+
+// if(userage >= 19 && userage <= 22){
+//     console.log("aip college student hu")
+// }
+
+
+// if(userage >= 23 && userage <= 25 ){
+//     console.log("aip Kaam Pa Jao")
+// }
+
+
+// var lunech = ["baryani", "qorma", "nahari", "befe", "chekan", "karahi", "sabzi"];
+
+// var days = [`sunday`, `monday`, `tusday`, `wenesday`, `tasday`, `friday`, `sataday`]
+
+
+// var days = prompt('aip ko kon se din ka kahni ka poch na ha');
+
+// if(days == "sunday"){
+//     alert("aaj sunday  ka din ha aaj baryani bani haa")
+// }
+
+
+// if(days == "monday" ){
+//     alert("aaj monday  ka din ha aaj qorma bani haa")
+// }
+// if(days == "tusday" ){
+//     alert("aaj tusday  ka din ha aaj nahari bani haa")
+// }
+
+// if(days == "wenesday" ){
+//     alert("aaj wenesday  ka din ha aaj qorma bani haa")
+// }
+
+// if(days == "tasday" ){
+//     alert("aaj tasday  ka din ha aaj qorma bani haa")
+// }
+
+// if(days == "friday" ){
+//     alert("aaj friday  ka din ha aaj qorma bani haa")
+// }
+// if(days == "sataday" ){
+//     alert("aaj sataday  ka din ha aaj  bani haa")
+// }
+
+
+
+// var userName = prompt("Aip Ka Kia Name Ha");
+// var userage = prompt("Aip Ka Age Kia Ha");
+
+
+// if(userage >= 18){
+//     alert("Aip Ok Hu" + " " + userName )
+// }
+
+// if(userage < 18){
+//     alert("Aip Chote Hu" + " " + userName )
+// }
+
+// var name = prompt("Whats Your Name")
+// var age = prompt("Gender")
+
+
+// if(age == famle){
+//     console.log(
+
+
+//     "Thank You Sir"
+//     )
+
+// }else{
+//     console.log(
+//         "Thank You Miss"
+//     )
+// }
+
+// function juice(name, name2){
+// if(name2){
+//     console.log( name  + " or " + name2 + " cut de peses")
+//     console.log(  "on the jusermation")
+//     console.log("make a fresh juice " + name  + " or " + name2 )
+
+// }else{
+//     console.log( name  +  " cut de peses")
+//     console.log(  "on the jusermation")
+//     console.log("make a fresh juice " + name  )
+// }
+// }
+//  juice( "mango")
+
+
+
+// function functionBtn() {
+//     var userAge = prompt("Pleas Enter Your Age");
+//     if (userAge <= 12 && userAge > 5) {
+//         alert(`Aip ${12 - userAge} Chotay Hu Aip ${12 - userAge} Saal Entezar Karo `)
+//     } else if (userAge <= 4 && userAge >= 2) {
+//         alert(`Aip Tu Bachy Hu ${12 - userAge} Entezaar Karo`)
+//     } else if (userAge < 2) {
+//         alert(`You Dont Allowed`)
+//     } else if (userAge > 12 && userAge < 16) {
+//         var schoolstudent = prompt(`Kia Aip School Student Hu ` + " yes' " + " No, ")
+//         if (schoolstudent === "yes") {
+//             var wichscool = prompt('Aip Konsa School Ma Hu "Gvt," Pravet,')
+//             if (wichscool == "gov") {
+//                 alert('ka aip chalo ga')
+//             } else {
+//                 alert('Pravet School Par Hame Barwase Na He Ha Q ka ma KHud Pravet School ma paray ha')
+//             }
+//         } else {
+//             alert(`Aip Ok Hu Lekeen TAleem jarey rako`)
+//         }
+
+//     } else if (userAge > 16 && userAge < 18){
+        
+//     }
+
+// }
+                                                                                    // Chapter no 12 
+
+// var times = prompt ("Time")
+
+
+// if(times < 1200   && times >= 0 ){
+//     alert("GOOD MORNING")
+// }
+
+
+// if(times >= 1200   && times < 1700 ){
+//     alert("GOOD AFTARNON")
+// }
+
+
+// if(times >= 1700   && times < 2100 ){
+//     alert("GOOD EVENING")
+// }
+
+
+
+// if(times >= 2100   && times <= 2359 ){
+//     alert("GOOD NIGHT")
+// }
+
+                                                                               // Chapter no 12 
+// var times = prompt("time slicet 24 houre");
+
+
+// if(times <= 24 && times > 17){
+//     alert("Good Night");
+// }else if(times <= 17 && times > 13){
+//     alert("Good Evening")
+// }else if(times <= 13 && times > 8){
+//     alert("Good Aftarnon")
+// }
+// else{
+//     alert("Good Moorning");
+// }
+                                                     //  Chapter no 12 
+
+// function buttoms(){
+
+    
+// var times = prompt("time slicet 24 houre");
+
+// if(times <= 24 && times > 17){
+//     alert("Good Night");
+// }else if(times <= 17 && times > 13){
+//     alert("Good Evening")
+// }else if(times <= 13 && times > 8){
+//     alert("Good Aftarnon")
+// }else if(times <= 8 && times > 5){
+//     alert("Good Morning")
+// }
+// else{
+//     alert("Good Day");
+// }
+
+
+// }
+
+
+
+
+                                                     //   Chapter no 12 
+// var greeting;
+// var houre = 19;
+// if(houre < 18){
+//     greeting = "good day"
+// }else{
+//     greeting = "good evening"
+// }
+// console.log(greeting)
+                                                       // Chapter no 12 
+
+// var password = prompt("Enter password")
+// var ConfromPassword = prompt("Enter Confrom password")
+// if(password == ConfromPassword){
+//     alert("You Are Sucsessfully")
+// }else{
+//     alert("Your Password Are Not match")
+// }
+//                                                            // chapter no 12 wovel Q 4
+// var wovel = prompt("Writ a Vowel a one charecter")
+
+// if(wovel == 'a' ){
+//     alert('its true')
+// }else if(wovel == 'e' ){
+//     alert('its true') 
+// }else if(wovel == 'i' ){
+//     alert('its true')
+// }else if(wovel == 'o' ){
+//     alert('its true')
+// }else if(wovel == 'u' ){
+//     alert('its true')
+// }
+//  else{
+//     alert('its  fulse ,,,, its not allowed')
+// }
+
+// var numbar = prompt('Enter ')
+
+
+
+// var student = {
+//   name : "khan",
+//   age : 20,
+//   numbar : null,
+// }
+
+// console.log(student['numbar'];
+
+
+                                                                //  tarnery oprat 
+// let age = 12;
+
+// let newage =  (age > 10) ? "ok age":"not ok"
+// console.log(newage)
+
+
+// debugger
+
+// let age = 33
+// let newage = (age >= 34)?
+//  "Are you not allowed ":
+//  "your allowed";
+// console.log(newage)
+
+
+
+                                                  //calculate
+
+
+
+
+// var num1 = +prompt('Enter valuo 1')
+// var num2 = +prompt('Enter valuo 2')
+
+// var add = num1 + num2
+
+
+// document.write(add)
+
+
+
+
+
+//                                                       // marksheet
+
+
+
+// var name = prompt('Enter Your Name','khan');
+// var rollNo = +prompt('Enter Your Roll No','776895');
+
+//  var english = +prompt('Enter Your English Marks');
+// var math = +prompt('Enter Your math Marks');
+//  var urdu = +prompt('Enter Your Urdu Marks');
+//  var islamait = +prompt('Enter Your Islamait Marks');
+//  var pysices = +prompt('Enter Your Pysices Marks');
+
+
+//  var totalMarks = english + math + urdu + islamait + pysices ;
+
+// var persentage = totalMarks/500*100
+
+// if(totalMarks <= "500" && totalMarks >= '460'){ 
+//     document.write(`
+// <table border="1" align='center'>
+
+// <tr>
+// <th colspan='2'> Name  : ${name} // Roll No  : ${rollNo} </th>
+// </tr>
+
+// <tr><td> English </td><td>${english}</td></tr>
+
+// <tr><td> Math </td><td>${math}</td></tr>
+
+// <tr><td> Urdu </td><td>${urdu}</td></tr>
+
+// <tr><td> Islamait </td><td>${islamait}</td></tr>
+
+// <tr><td> Pysicses </td><td>${pysices}</td></tr>
+// <tr><td>Total Marks</td> <td>  ${totalMarks} /   % ${persentage} A+</td></tr>
+// </table>
+// `)
+// }
+
+
+// else if(totalMarks < "460" && totalMarks >= '410'){ 
+//     document.write(`
+// <table border="1" align='center'>
+
+// <tr>
+// <th colspan='2'> Name  : ${name} // Roll No  : ${rollNo} </th>
+// </tr>
+
+// <tr><td> English </td><td>${english}</td></tr>
+
+// <tr><td> Math </td><td>${math}</td></tr>
+
+// <tr><td> Urdu </td><td>${urdu}</td></tr>
+
+// <tr><td> Islamait </td><td>${islamait}</td></tr>
+
+// <tr><td> Pysicses </td><td>${pysices}</td></tr>
+// <tr><td>Total Marks</td> <td>  ${totalMarks} /   % ${persentage} A</td></tr>
+// </table>
+// `)
+// }
+
+
+
+
+// else if(totalMarks < "410" && totalMarks >= '380'){ 
+//     document.write(`
+// <table border="1" align='center'>
+
+// <tr>
+// <th colspan='2'> Name  : ${name} // Roll No  : ${rollNo} </th>
+// </tr>
+
+// <tr><td> English </td><td>${english}</td></tr>
+
+// <tr><td> Math </td><td>${math}</td></tr>
+
+// <tr><td> Urdu </td><td>${urdu}</td></tr>
+
+// <tr><td> Islamait </td><td>${islamait}</td></tr>
+
+// <tr><td> Pysicses </td><td>${pysices}</td></tr>
+// <tr><td>Total Marks</td> <td>  ${totalMarks} /   % ${persentage} B</td></tr>
+// </table>
+// `)
+// }
+
+
+
+// else if(totalMarks < "380" && totalMarks >= '350'){ 
+//     document.write(`
+// <table border="1" align='center'>
+
+// <tr>
+// <th colspan='2'> Name  : ${name} // Roll No  : ${rollNo} </th>
+// </tr>
+
+// <tr><td> English </td><td>${english}</td></tr>
+
+// <tr><td> Math </td><td>${math}</td></tr>
+
+// <tr><td> Urdu </td><td>${urdu}</td></tr>
+
+// <tr><td> Islamait </td><td>${islamait}</td></tr>
+
+// <tr><td> Pysicses </td><td>${pysices}</td></tr>
+// <tr><td>Total Marks</td> <td>  ${totalMarks} /   % ${persentage} C </td></tr>
+// </table>
+// `)
+// }
+
+
+
+// else if(totalMarks < "350" && totalMarks >= '310'){ 
+//     document.write(`
+// <table border="1" align='center'>
+
+// <tr>
+// <th colspan='2'> Name  : ${name} // Roll No  : ${rollNo} </th>
+// </tr>
+
+// <tr><td> English </td><td>${english}</td></tr>
+
+// <tr><td> Math </td><td>${math}</td></tr>
+
+// <tr><td> Urdu </td><td>${urdu}</td></tr>
+
+// <tr><td> Islamait </td><td>${islamait}</td></tr>
+
+// <tr><td> Pysicses </td><td>${pysices}</td></tr>
+// <tr><td>Total Marks</td> <td>  ${totalMarks} /   % ${persentage} D</td></tr>
+// </table>
+// `)
+// }
+
+
+
+// else if(totalMarks < "310" && totalMarks >= '290'){ 
+//     document.write(`
+// <table border="1" align='center'>
+
+// <tr>
+// <th colspan='2'> Name  : ${name} // Roll No  : ${rollNo} </th>
+// </tr>
+
+// <tr><td> English </td><td>${english}</td></tr>
+
+// <tr><td> Math </td><td>${math}</td></tr>
+
+// <tr><td> Urdu </td><td>${urdu}</td></tr>
+
+// <tr><td> Islamait </td><td>${islamait}</td></tr>
+
+// <tr><td> Pysicses </td><td>${pysices}</td></tr>
+// <tr><td>Total Marks</td> <td>  ${totalMarks} /   % ${persentage} E</td></tr>
+// </table>
+// `)
+// }
+
+
+
+
+
+// else if(totalMarks < "290" ){ 
+//     document.write(`
+// <table border="1" align='center'>
+
+// <tr>
+// <th colspan='2'> Name  : ${name} // Roll No  : ${rollNo} </th>
+// </tr>
+
+// <tr><td> English </td><td>${english}</td></tr>
+
+// <tr><td> Math </td><td>${math}</td></tr>
+
+// <tr><td> Urdu </td><td>${urdu}</td></tr>
+
+// <tr><td> Islamait </td><td>${islamait}</td></tr>
+
+// <tr><td> Pysicses </td><td>${pysices}</td></tr>
+// <tr><td>Total Marks</td> <td>  ${totalMarks} /   % ${persentage} Fail </td></tr>
+// </table>
+// `)
+// }
+// else{
+    
+// }
+
+// else{
+//     alert('villed Number')
+// }
+// document.write(`
+// <table border="1" align='center'>
+
+// <tr>
+// <th colspan='2'> Name  : ${name} // Roll No  : ${rollNo} </th>
+// </tr>
+
+
+// <tr><td> English </td><td>${english}</td></tr>
+
+// <tr><td> Math </td><td>${math}</td></tr>
+
+// <tr><td> Urdu </td><td>${urdu}</td></tr>
+
+// <tr><td> Islamait </td><td>${islamait}</td></tr>
+
+// <tr><td> Pysicses </td><td>${pysices}</td></tr>
+// <tr><td>Total Marks</td> <td>  ${totalMarks} /   % ${persentage}</td></tr>
+// </table>
+// `)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
